223b5c5a2817f71143431240a2331ae14721a9dd
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-14 12:06:24 -0600

    Add slug to factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user, class: User do
     sequence(:email) { |n| "user_#{n}@gmail.com" }
+    sequence(:slug) { |n| "user_#{n}-gmail.com" }
     password { "password" }
     sequence(:name) { |n| "User Name #{n}" }
     sequence(:address) { |n| "Address #{n}" }
@@ -13,17 +14,20 @@ FactoryBot.define do
   factory :inactive_user, parent: :user do
     sequence(:name) { |n| "Inactive User Name #{n}" }
     sequence(:email) { |n| "inactive_user_#{n}@gmail.com" }
+    sequence(:slug) { |n| "inactive_user_#{n}-gmail.com" }
     active { false }
   end
 
   factory :merchant, parent: :user do
     sequence(:email) { |n| "merchant_#{n}@gmail.com" }
+    sequence(:slug) { |n| "merchant_#{n}-gmail.com" }
     sequence(:name) { |n| "Merchant Name #{n}" }
     role { 1 }
     active { true }
   end
   factory :inactive_merchant, parent: :user do
     sequence(:email) { |n| "inactive_merchant_#{n}@gmail.com" }
+    sequence(:slug) { |n| "inactive_merchant_#{n}@-mail.com" }
     sequence(:name) { |n| "Inactive Merchant Name #{n}" }
     role { 1 }
     active { false }
@@ -31,6 +35,7 @@ FactoryBot.define do
 
   factory :admin, parent: :user do
     sequence(:email) { |n| "admin_#{n}@gmail.com" }
+    sequence(:slug) { |n| "admin_#{n}-gmail.com" }
     sequence(:name) { |n| "Admin Name #{n}" }
     role { 2 }
     active { true }
