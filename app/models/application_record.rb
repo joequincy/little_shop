fcc87a5387cd5eb985e2e4b5554d91068d7191d5
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-15 11:23:17 -0600

    Add pie chart support and State chart

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  module ChartSupport
+    def to_pie_columns(data)
+      self.map do |record|
+        {name: record[data.keys.first], data: record[data.values.first]}
+      end
+    end
+  end
+  ActiveRecord::AssociationRelation.send(:include, ApplicationRecord::ChartSupport)
 end
