41f2d832f1367a482ef825dfd0347f7304cda521
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-15 13:42:55 -0600

    Further expansion of seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,76 @@
 require 'factory_bot_rails'
 
+@time_rng = Random.new(104771926725897930682341174863059830501)
+def rand_time(from: 0.0, to: Time.now)
+  Time.at(@time_rng.rand(from..to))
+end
+
+def order_stats(items, created_at, updated_at)
+  item = items.sample
+  items.delete(item)
+  updated = rand_time(from: created_at, to: updated_at)
+  return item, items, updated
+end
+
 include FactoryBot::Syntax::Methods
 
-# OrderItem.destroy_all
-# Order.destroy_all
+OrderItem.destroy_all
+Order.destroy_all
 Item.destroy_all
 User.destroy_all
 
-admin = create(:admin)
-user = create(:user)
-users = create_list(:user, 10)
-merchant_1 = create(:merchant)
+site_launched = rand_time(from: 3.years.ago, to: 2.years.ago)
 
-merchant_2, merchant_3, merchant_4 = create_list(:merchant, 3)
+admin = create(:admin)
+users = create_list(:user, 20)
+merchants = create_list(:merchant, 8)
 
 inactive_merchant_1 = create(:inactive_merchant)
 inactive_user_1 = create(:inactive_user)
 
-item_1 = create(:item, user: merchant_1)
-item_2 = create(:item, user: merchant_2)
-item_3 = create(:item, user: merchant_3)
-item_4 = create(:item, user: merchant_4)
-items = create_list(:item, 10, user: merchant_1)
-base_items = [item_1, item_2, item_3, item_4]
+items = []
+(1..160).each do |i|
+  items << create(:item, user: merchants.sample, created_at: site_launched, updated_at: site_launched)
+end
 
-inactive_item_1 = create(:inactive_item, user: merchant_1)
+inactive_item_1 = create(:inactive_item, user: merchants[0])
 inactive_item_2 = create(:inactive_item, user: inactive_merchant_1)
 
 Random.new_seed
 rng = Random.new
 
-order = create(:shipped_order, user: user)
-create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: (rng.rand(3)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: (rng.rand(5)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
+(1..100).each do |i|
+  created_at = rand_time(from: 2.years.ago)
+  updated_at = rand_time(from: created_at)
+  order = create(:shipped_order, user: users.sample, created_at: created_at, updated_at: updated_at)
+  potential_items = items.dup
+  (1..rng.rand(6)).each do |j|
+    item, potential_items, updated_at = order_stats(potential_items, created_at, updated_at)
+    create(:fulfilled_order_item, order: order, item: item, price: rng.rand(1..20), quantity: rng.rand(1..5), created_at: created_at, updated_at: updated_at)
+  end
+end
 
-# pending order
-order = create(:order, user: user)
-create(:order_item, order: order, item: item_1, price: 1, quantity: 1)
-create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: (rng.rand(23)+1).days.ago, updated_at: rng.rand(23).hours.ago)
+(1..10).each do |i|
+  created_at = rand_time(from: 2.years.ago)
+  updated_at = rand_time(from: created_at)
+  order = create(:order, user: users.sample, created_at: created_at, updated_at: updated_at)
+  potential_items = items.dup
+  (1..rng.rand(6)).each do |j|
+    item, potential_items, updated_at = order_stats(potential_items, created_at, updated_at)
+    item_status = [:fulfilled_order_item, :order_item][rng.rand(0..1)]
+    create(:fulfilled_order_item, order: order, item: item, price: rng.rand(1..20), quantity: rng.rand(1..5), created_at: created_at, updated_at: updated_at)
+  end
+end
 
-order = create(:cancelled_order, user: user)
-create(:order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-create(:order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-
-order = create(:shipped_order, user: user)
-create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: (rng.rand(4)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
-create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-
-(1..15).each do |i|
-  order = create(:shipped_order, user: users.sample)
-  create(:fulfilled_order_item, order: order, item: items.sample, price: 1, quantity: 1, created_at: (rng.rand(4)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
-  create(:fulfilled_order_item, order: order, item: base_items.sample, price: 1, quantity: 1, created_at: (rng.rand(4)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
+(1..40).each do |i|
+  created_at = rand_time(from: 2.years.ago)
+  updated_at = rand_time(from: created_at)
+  order = create(:cancelled_order, user: users.sample, created_at: created_at, updated_at: updated_at)
+  potential_items = items.dup
+  (1..rng.rand(6)).each do |j|
+    item, potential_items, updated_at = order_stats(potential_items, created_at, updated_at)
+    create(:order_item, order: order, item: item, price: rng.rand(1..20), quantity: rng.rand(1..5), created_at: created_at, updated_at: updated_at)
+  end
 end
 
 
