c45991d75badc70e5344334954dee1ed06a4b494
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-16 08:40:22 -0600

    Add instance variable for sales_history

--- app/controllers/dashboard/dashboard_controller.rb
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Dashboard::DashboardController < Dashboard::BaseController
   def index
     @merchant = current_user
+    @sales_history = @merchant.sales_history.to_timeseries(:month, :sales)
     @pending_orders = Order.pending_orders_for_merchant(current_user.id)
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -7,6 +7,17 @@ class ApplicationRecord < ActiveRecord::Base
         {name: record[data.keys.first], data: record[data.values.first]}
       end
     end
+
+    def to_timeseries(date, value)
+      x = ['dates']
+      y = ['values']
+      self.each do |record|
+        x << record[date].strftime("%Y-%m-%d")
+        y << record[value].to_f.round(2)
+      end
+      return x, y
+    end
   end
   ActiveRecord::AssociationRelation.send(:include, ApplicationRecord::ChartSupport)
+  ActiveRecord::Relation.send(:include, ApplicationRecord::ChartSupport)
 end
